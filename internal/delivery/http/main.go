60cf9ec44415db190210e4172418116340d7331f
Fajar-Islami <ahmadfajarislami@protonmail.com>
2023-03-21 01:02:04 +0700

    :hammer: auto deploy cicd

--- internal/delivery/http/main.go
+++ internal/delivery/http/main.go
@@ -39,7 +39,7 @@ func HTTPRouteInit(cont *container.Container) {
 	port := fmt.Sprintf("%s:%d", cont.Apps.Host, cont.Apps.HttpPort)
 	docs.SwaggerInfo.Host = fmt.Sprintf("%s/api/v1", port)
 	utils.InitSnowflake()
-
+	fmt.Println("app running on port : ", port)
 	api := e.Group("/api/v1") // /api
 	api.Any("", HealthCheck)
 	api.Any("/health", HealthCheck)
